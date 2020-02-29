ad388dc790daab93785b340eac14d3f5aa5c285a
rahul yadav <rahulyadav5524@gmail.com>
2020-02-29 15:33:10 +0530

    color display final

--- color_sensor_display/color_sensor_display.ino
+++ color_sensor_display/color_sensor_display.ino
@@ -5,16 +5,6 @@ int array[4]={1,0,1,1};
 int speeed =(array[0]*8+array[1]*4+array[2]*2+array[3])/5;
 int angle = ((array[0]*8+array[1]*4+array[2]*2+array[3])*10);
 
-
-
-//const int s0=47;
-//const int s1=48;
-//const int s2=49;
-//const int s3=50;
-//const int out=51;
-//const int vcc=52;
-//const int led=53;
-
 const int s0=3;
 const int s1=4;
 const int s2=5;
@@ -23,6 +13,7 @@ const int out=7;
 const int vcc=8;
 const int led=9;
 int red,green,blue;
+int x;
 void setup() {
   pinMode(11,HIGH);
   pinMode(2,LOW);
@@ -47,104 +38,93 @@ void lcd_display(int a)
   lcd.init();
   // Print a message to the LCD.
   lcd.backlight();
-  if(a==-1)
-  {
-    lcd.setCursor(0,0);
-    lcd.print("MAT=");
-    for (int i=0;i<4;i++)
-    {   
-        lcd.setCursor(i+4,0);
-        lcd.print(array[i]);
-     }
-    lcd.setCursor(0,1);
-    lcd.print("speed=");
-    lcd.setCursor(6,1);
-    lcd.print(speeed);
-    lcd.setCursor(8,1);
-    lcd.print("angle=");
-    lcd.print(angle);
-    }
-    else if(a==0)
-    {
-        lcd.setCursor(0,0);
-        lcd.print("red");
-    }
-    else if(a==1)
-    {
-        lcd.setCursor(0,0);
-        lcd.print("green");
-     }
-    else if(a==2)
-     {
-        lcd.setCursor(0,0);
-         lcd.print("blue");
+//  if(a==-1)
+//  {
+//    lcd.setCursor(0,0);
+//    lcd.print("MAT=");
+//    for (int i=0;i<4;i++)
+//    {   
+//        lcd.setCursor(i+4,0);
+//        lcd.print(array[i]);
+//     }
+//    lcd.setCursor(0,1);
+//    lcd.print("speed=");
+//    lcd.setCursor(6,1);
+//    lcd.print(speeed);
+//    lcd.setCursor(8,1);
+//    lcd.print("angle=");
+//    lcd.print(angle);
+//    }
+    if(a==0)
+      {
+          lcd.setCursor(0,0);
+          lcd.print("red");
       }
-    else{
+    else if(a==1)
+      {
+          lcd.setCursor(0,0);
+          lcd.print("green");
+      }
+    else if(a==2)
+      {
+          lcd.setCursor(0,0);
+          lcd.print("blue");
+      }
+    else if(a==-1){
         lcd.setCursor(0,0);
         lcd.print("NOTHING");
       }
 }
 
 int getdata(){
-     lcd.init(); 
-     lcd.backlight();  
-    digitalWrite(s2,LOW);
-    digitalWrite(s3,LOW);
-    red=pulseIn(out,LOW);
-  Serial.print("red:");
-  Serial.print(red);
-  Serial.print(" ");
-//    delay(500);
-    digitalWrite(s2,LOW);
-    digitalWrite(s3,HIGH);
-    blue=pulseIn(out,LOW);
-  Serial.print("blue:");
-  Serial.print(blue);
-  Serial.print(" ");
-//    delay(500);
-    digitalWrite(s2,HIGH);
-    digitalWrite(s3,HIGH);
-    green=pulseIn(out,LOW);
-  Serial.print("green:");
-  Serial.println(green);
-//    delay(500);
-int x;
-    if(red<green && red<blue && red<200)
-    {
-      Serial.println("Red");
-//    Serial.println(" ");
-     // lcd.setCursor(0,0);
-      //lcd.print("RED");
-      x=0;
-    }
-    else if(green<red && green<blue && green<200)
-    {
-      Serial.println("Green");
-      Serial.println(" ");
-     // lcd.setCursor(0,0);
-      //lcd.print("GREEN");
-      x=1;
-    }
+      lcd.init(); 
+      lcd.backlight();  
+      digitalWrite(s2,LOW);
+      digitalWrite(s3,LOW);
+      red=pulseIn(out,LOW);
+      Serial.print("red:");
+      Serial.print(red);
+      Serial.print(" ");
+      digitalWrite(s2,LOW);
+      digitalWrite(s3,HIGH);
+      blue=pulseIn(out,LOW);
+      Serial.print("blue:");
+      Serial.print(blue);
+      Serial.print(" ");
+      digitalWrite(s2,HIGH);
+      digitalWrite(s3,HIGH);
+      green=pulseIn(out,LOW);
+      Serial.print("green:");
+      Serial.println(green);
+      if(red<green && red<blue && red<200)
+        {
+          lcd.setCursor(0,0);
+          lcd.print("RED");
+          x=0;
+         }
+      else if(green<red && green<blue && green<200)
+        {
+          lcd.setCursor(0,0);
+          lcd.print("GREEN");
+          x=1;
+        }
     else if(blue<red && blue<red && blue<200)
     {
-      Serial.println("Blue");
-      Serial.println(" ");
-      //lcd.setCursor(0,0);
-      //lcd.print("BLUE");
-      x=2;
+        lcd.setCursor(0,0);
+        lcd.print("BLUE");
+        x=2;
     }
-    else{
-      Serial.println("NOTHING");
-      //lcd.setCursor(0,0);
-      //lcd.print("NOTHING");
-      x=-1;
+    else
+    {
+        lcd.setCursor(0,0);
+        lcd.print("NOTHING");
+        x=-1;
     }
     return x;
 }
-void loop() {
-  int a;
- a=getdata();
- lcd_display(a);
-  //delay(10);
-
+void loop() 
+{
+    int a;
+    a=getdata();
+    lcd_display(a);
 }//remaining returnvalue of ......
